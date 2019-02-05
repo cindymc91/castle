5091f6375c42950f546694ba303dd85fdfc96621
cindymc91 <cindy.mc@live.fr>
2019-02-05 22:37:00 +0100

    Main module to connect 'catle' and 'michelin'

--- code/index.js
+++ code/index.js
@@ -1,7 +1,8 @@
-const request_promise = require('request-promise');
-const cheerio = require('cheerio');
+const castle = require('./castle');
+const michelin = require('./michelin')
 
-const options = {
+
+/*const options = {
   uri: 'https://www.relaischateaux.com/fr/site-map/etablissements',
   transform: function (body) {
     return cheerio.load(body);
@@ -16,15 +17,33 @@ request_promise(options)
 
     var test =[];
 
-    $('#countryF + #countryF').children('.listDiamond').children('li').children('a').each(function(i,elm){
+    $('#countryF + #countryF').children('.listDiamond').children('li').each(function(i,elm){
       //test[i] = $(this).text(); //To have the text (name of the hotel)
-      test[i] = $(this).attr('href'); //To have the link
+      test[i] = $(this).children('a').first().attr('href'); //Only have the 1st link
+      //test[i] = $(this).attr('href'); //To have the link
     });
-
-    //Les établissements avec juste restaurant sont à enlever
-
-    console.log(test[1]);
+    console.log(test);
   })
   .catch((err) => {
     console.log(err);
-  });
+  });*/
+
+let tabWithAllurl;
+let tabHotelRestaurant;
+let starredResto;
+
+async function start(){
+  //On récupère la partie Relaix Chateaux
+  //tabWithAllurl = await castle.grabAllurl('https://www.relaischateaux.com/fr/site-map/etablissements');
+  //console.log(tabWithAllurl.length);
+  //tabHotelRestaurant = await castle.checkHotelRestaurant(tabWithAllurl);
+  //console.log(newTab.length);
+  //console.log(tabWithAllurl.length);
+
+  //On récupère la partie Michelin
+  starredResto = await michelin.loopThroughPages();
+  console.log(starredResto[625]);
+
+}
+
+start();
